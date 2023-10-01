76f268103211487ee0d3ed31c4329957a784bd62
samuelmeira <slmeira.dev@gmail.com>
2023-10-01 19:54:08 -0300

    feat: setup redis

---
+++ src/queues/notifications.ts
@@ -0,0 +1,14 @@
+import { Queue } from 'bullmq';
+
+import { REDIS_HOST, REDIS_PORT } from '../config/redis';
+
+const notificationsQueue = new Queue('Notifications',
+{
+  connection: {
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  }
+})
+
+
+export default notificationsQueue;
