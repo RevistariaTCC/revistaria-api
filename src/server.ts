35921002335a561fea8296efdfa528b4480b04d2
samuelmeira <slmeira.dev@gmail.com>
2023-10-09 19:26:14 -0300

    fix: adjust port

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ new Worker('Notifications', createNotifications, {
 });
 
 
-app.listen({ host: '0.0.0.0', port: 4000 }, (err, address) => {
+app.listen({ host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 3000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
