902c88d9c32bd6d926d77da9718f6ba1f018f76c
samuelmeira <slmeira.dev@gmail.com>
2023-11-21 00:52:22 -0300

    feat: add update notification to read

--- src/services/notifications/ReadNotificationById.ts
+++ src/services/notifications/ReadNotificationById.ts
@@ -1,17 +1,20 @@
+import { User } from '@prisma/client';
 import prisma from '../../adapters/prisma-adapter';
 import AppError from '../../errors/AppError';
 
 class ReadNotificationById {
-  public async execute(id: string) {
+  public async execute(id: string, user: User) {
     try {
       const notificationFind = await prisma.notification.findUnique({
         where: {
-          id: id
+          id: id,          
         }
+
       });
 
       if (!notificationFind) throw new AppError('Notification not found.', 404);
 
+      if(notificationFind.userId !== user.id)  throw new AppError('Notification not found.', 404);
       return await prisma.notification.update({
         where: {
           id
